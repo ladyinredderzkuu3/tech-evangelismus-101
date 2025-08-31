f88b41316871b5d65ba59402873f9cde758fec57
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-31 03:45:05 -0500

    Add comments to the Express server setup

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Define a route for the root URL that serves the index.html file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}`);
 });
