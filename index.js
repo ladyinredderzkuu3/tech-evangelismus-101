44462eb205d19dce9d19764d871193f3dd1ceab2
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-31 03:50:07 -0500

    Add comments to clarify the code functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 
 // Define a route for the root URL that serves the index.html file
 app.get('/', (req, res) => {
+  // Use sendFile to send the index.html file located in the public directory
   res.sendFile(__dirname + '/public/index.html');
 });
 
